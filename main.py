1f0ff9abd19a0138362b12486019ab6226716702
Bryvkin Daniel <38356534+WWWorm92@users.noreply.github.com>
2020-11-28 19:04:24 +0300

    debug2

--- main.py
+++ main.py
@@ -1,49 +1,70 @@
 import tkinter as tk
 from tkinter import ttk
 
-def set_values():
+destroy = []
+
+
+def set_values(window):
+    global res
     txt1 = int(e1.get())
     txt2 = int(e2.get())
     txt3 = int(e3.get())
-    print(convert(txt1, txt2, txt3))
+    res = convert(txt1, txt2, txt3)
+    graphic(window)
 
-def graphic():
-    window = tk.Tk()
-    window.title("Лабораторная работа №1")
-    window.geometry('400x200')
-    window.resizable(width=False, height=False)
+
+def graphic(window):
+    global res, destroy_objects
+    for i in destroy_objects:
+        i.destroy()
+    destroy_objects = []
     tab_control = ttk.Notebook(window)
     tab1 = ttk.Frame(tab_control)
+    destroy_objects.append(tab1)
     tab2 = ttk.Frame(tab_control)
+    destroy_objects.append(tab2)
     tab_control.add(tab1, text='Программа')
+    destroy_objects.append(tab_control)
     tab_control.add(tab2, text='Авторы')
+    destroy_objects.append(tab_control)
     window["bg"] = "lightgray"
     lbl1 = tk.Label(tab1, text="Введите число", )
+    destroy_objects.append(lbl1)
     lbl1.grid(column=0, row=0)
     global e1
     e1 = tk.Entry(tab1, text="")
+    destroy_objects.append(e1)
     e1.grid(column=1, row=0)
-    txt1 = e1.get()
+    # txt1 = e1.get()
     lbl2 = tk.Label(tab1, text="В какую СС:")
+    destroy_objects.append(lbl2)
     lbl2.grid(column=0, row=1)
     global e2
     e2 = tk.Entry(tab1, text="")
+    destroy_objects.append(e2)
     e2.grid(column=1, row=1)
     lbl3 = tk.Label(tab1, text="Из какой СС:")
+    destroy_objects.append(lbl3)
     lbl3.grid(column=0, row=2)
     global e3
     e3 = tk.Entry(tab1, text="")
+    destroy_objects.append(e3)
     e3.grid(column=1, row=2)
-    btn = tk.Button(tab1, text="OK", command=set_values)
+    btn = tk.Button(tab1, text="OK", command=lambda: set_values(window))
+    destroy_objects.append(btn)
     btn.grid(column=1, row=4)
     lbl4 = tk.Label(tab1, text="Результат:")
+    destroy_objects.append(lbl4)
     lbl4.grid(column=0, row=3)
+    lbl5 = tk.Label(tab1, text=res)
+    destroy_objects.append(lbl5)
+    lbl5.grid(column=1, row=3)
     lblgroup = tk.Label(tab2,
                         text="Работу выполнили студенты группы 0307: \n Брывкин Даниил \n Верещагин Роман \n Обрезков "
                              "Егор")
     lblgroup.grid(column=0, row=8)
     tab_control.pack(expand=1, fill='both')
-    print(txt1)
+    # print(txt1)
     window.mainloop()
 
 
@@ -68,7 +89,12 @@ def output():
     print(convert(number, to_, from_))
 
 
-graphic()
+destroy_objects = []
+res = ''
+window = tk.Tk()
+window.title("Лабораторная работа №1")
+window.geometry('400x200')
+window.resizable(width=False, height=False)
+graphic(window)
 
-
-#output()
+# output()
